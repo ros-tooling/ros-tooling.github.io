3f6e959e57fd4b4eca2f12433bd02dd61593ab18
Emerson Knapp <emerson.b.knapp@gmail.com>
2020-08-18 18:26:10 -0700

    Updates
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

---
+++ precache-manifest.f83c3c8a6f7d4e4341470d3ecfe081a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6e6b7e2cbb56f58fd0b9a32fb8d57a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8957d3d1872395d1d89",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ae338e9dcd78722ced46",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e8957d3d1872395d1d89",
+    "url": "/static/js/2.1cb0f870.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1cb0f870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae338e9dcd78722ced46",
+    "url": "/static/js/main.05f2408a.chunk.js"
+  },
+  {
+    "revision": "3570c61aaf0c6db2a4b4",
+    "url": "/static/js/runtime-main.e55f9afb.js"
+  }
+]);
