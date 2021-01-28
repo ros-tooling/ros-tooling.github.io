6bc66f9753e70d31b8d83124c88bf78daafe6fd9
Emerson Knapp <eknapp@amazon.com>
2021-01-28 12:56:11 -0800

    Add Rolling support
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>

--- src/App.js
+++ src/App.js
@@ -18,27 +18,12 @@ const latest_packages = {
   "ros-tooling/libstatistics_collector": {},
   "ros-tooling/setup-ros": {},
   "ros-tooling/setup-ros-docker": {"workflow_override": "Build Docker image", "no_codecov": true},
-  "ros-tooling/system_metrics_collector": {"nightly_workflow": "End-to-end Testing (Nightly)"},
+  "ros-tooling/system_metrics_collector": {},
   "ros2/rosbag2": {"no_codecov": true},
   "ros2/rosbag2_bag_v2": {"no_codecov": true},
 };
 
 const dashing_packages = {
-  "ros2bag": {'no_dev_build': true},
-  "rosbag2": {},
-  "rosbag2_converter_default_plugins": {'no_dev_build': true},
-  "rosbag2_storage": {'no_dev_build': true},
-  "rosbag2_storage_default_plugins": {'no_dev_build': true},
-  "rosbag2_test_common": {'no_dev_build': true},
-  "rosbag2_tests": {'no_dev_build': true},
-  "rosbag2_transport": {'no_dev_build': true},
-  "shared_queues_vendor": {'no_dev_build': true},
-  "sqlite3_vendor": {'no_dev_build': true},
-  "ros1_rosbag_storage_vendor": {'no_dev_build': true},
-  "rosbag2_bag_v2_plugins": {'no_dev_build': true},
-};
-
-const eloquent_packages = {
   "ros2bag": {'no_dev_build': true},
   "rosbag2": {},
   "rosbag2_converter_default_plugins": {'no_dev_build': true},
@@ -73,10 +58,24 @@ const foxy_packages = {
   "system_metrics_collector": {},
 };
 
-const ros_distro_to_ubuntu_distro = {
-  "dashing": "bionic",
-  "eloquent": "bionic",
-  "foxy": "focal",
+const rolling_packages = {
+  "ros2bag": {"no_dev_build": true},
+  "rosbag2": {},
+  "rosbag2_compression": {"no_dev_build": true},
+  "rosbag2_converter_default_plugins": {"no_dev_build": true},
+  "rosbag2_cpp": {"no_dev_build": true},
+  "rosbag2_py": {"no_dev_build": true},
+  "rosbag2_storage": {"no_dev_build": true},
+  "rosbag2_storage_default_plugins": {"no_dev_build": true},
+  "rosbag2_test_common": {"no_dev_build": true},
+  "rosbag2_tests": {"no_dev_build": true},
+  "rosbag2_transport": {"no_dev_build": true},
+  "shared_queues_vendor": {"no_dev_build": true},
+  "sqlite3_vendor": {"no_dev_build": true},
+  "zstd_vendor": {"no_dev_build": true},
+  "ros1_rosbag_storage_vendor": {"no_dev_build": true},
+  "rosbag2_bag_v2_plugins": {"no_dev_build": true},
+  "libstatistics_collector": {},
 };
 
 const Empty = (
@@ -181,50 +180,49 @@ function DevelopmentStatusTable() {
 }
 
 function Ros2BuildIcon({ distro, pkg, config, build_type }) {
-  // Some short-tag values for reference
-  // Ddev
-  // Dsrc_uB
-  // Dbin_uB64
-  // Dbin_ubv8_uBv8
-  // Dbin_ubhf_uBhf
-  // Fbin_ubv8_uFv8
-  const initial = distro[0].toUpperCase();
-  const ubuntu_distro = ros_distro_to_ubuntu_distro[distro];
-  const ubuntu_initial = ubuntu_distro[0].toUpperCase();
-  let short_tag = ``;
-  let long_tag = ``;
-
-
-  if (distro === "foxy" && build_type === "armhf") {
-    return Empty;
-  }
   if (build_type === "dev" && config.no_dev_build) {
     return Empty;
   }
 
-  switch (build_type) {
-    case "dev":
-      short_tag = `${initial}dev`;
-      long_tag = `ubuntu_${ubuntu_distro}_amd64`;
-      break;
-    case "src":
-      short_tag = `${initial}src_u${ubuntu_initial}`;
-      long_tag = `ubuntu_${ubuntu_distro}__source`;
-      break;
-    case "x86_64":
-      short_tag = `${initial}bin_u${ubuntu_initial}64`;
-      long_tag = `ubuntu_${ubuntu_distro}_amd64__binary`;
-      break;
-    case "aarch64":
-      short_tag = `${initial}bin_ubv8_u${ubuntu_initial}v8`;
-      long_tag = `ubuntu_${ubuntu_distro}_arm64__binary`;
-      break;
-    case "armhf":
-      short_tag = `${initial}bin_ubhf_u${ubuntu_initial}hf`;
-      long_tag = `ubuntu_${ubuntu_distro}_armhf__binary`;
-      break;
-    default:
-      break;
+  // Reference logic for tags. Complex enough and requiring hidden config files, so
+  // not easy to fully replicate
+  // https://github.com/ros-infrastructure/ros_buildfarm/blob/master/ros_buildfarm/common.py
+  const sl = (short, long) => { return [ short, long ]; };
+  const tags = {
+    "dev": {
+      "dashing": sl("Ddev", "ubuntu_bionic_amd64"),
+      "foxy": sl("Fdev", "ubuntu_focal_amd64"),
+      "rolling": sl("Rdev", "ubuntu_focal_amd64"),
+    },
+    "src": {
+      "dashing": sl("Dsrc_uB", "ubuntu_bionic__source"),
+      "foxy": sl("Fsrc_uF", "ubuntu_focal__source"),
+      "rolling": sl("Rsrc_uF", "ubuntu_focal__source"),
+    },
+    "x86_64": {
+      "dashing": sl("Dbin_uB64", "ubuntu_bionic_amd64__binary"),
+      "foxy": sl("Fbin_uF64", "ubuntu_focal_amd64__binary"),
+      "rolling": sl("Rbin_uF64", "ubuntu_focal_amd64__binary"),
+    },
+    "aarch64": {
+      "dashing": sl("Dbin_ubv8_uBv8", "ubuntu_bionic_arm64__binary"),
+      "foxy": sl("Fbin_ubv8_uFv8", "ubuntu_focal_arm64__binary"),
+      "rolling": sl("Rbin_ufv8_uFv8", "ubuntu_focal_arm64__binary"),
+    },
+    "armhf": {
+      "dashing": sl("Dbin_ubhf_uBhf", "ubuntu_bionic_armhf__binary"),
+      "foxy": undefined,
+      "rolling": undefined,
+    }
+  }
+
+  const shortlong = tags[build_type][distro];
+  let short_tag;
+  let long_tag;
+  if (shortlong === undefined) {
+    return Empty;
+  } else {
+    [short_tag, long_tag] = shortlong;
   }
 
   const build_url = `http://build.ros2.org/view/${short_tag}/job/${short_tag}__${pkg}__${long_tag}`;
@@ -278,12 +276,12 @@ function App() {
           <DevelopmentStatusTable />
         </Row>
         <Row>
-          <h1> Released Packages - Foxy Fitzroy</h1>
-          <DistroReleaseTable distro="foxy" packages={foxy_packages} />
+          <h1>Released Packages - Rolling Ridley</h1>
+          <DistroReleaseTable distro="rolling" packages={rolling_packages} />
         </Row>
         <Row>
-          <h1>Released Packages - Eloquent Elusor</h1>
-          <DistroReleaseTable distro="eloquent" packages={eloquent_packages} />
+          <h1> Released Packages - Foxy Fitzroy</h1>
+          <DistroReleaseTable distro="foxy" packages={foxy_packages} />
         </Row>
         <Row>
           <h1>Released Packages - Dashing Diademata</h1>
